86c661977ff477bc4acd6be399cc1ccf2a7e167b
League Student <league@iMac-7.attlocal.net>
2019-07-24 14:26:49 -0700

    Day 3

--- src/section2/StarShow.java
+++ src/section2/StarShow.java
@@ -19,7 +19,7 @@ robot.setSpeed(8);
 		// 6. Make a variable to hold the X position of the Robot and set it to 10
 int robotx=10; 
 		// 7. Make a variable to hold the Y position of the Robot and set it to 600
-
+int roboty=600;
 		// 8. Make a variable to hold the star size and set it to 25
 
 		// 12. Use a for loop to repeat steps #19 to #18, 30 times
@@ -55,9 +55,9 @@ for(int i=0; i<5; i++) {
 	robot.turn(144);
 	}
 	
-	public static void main(String[] args) {
+
 		new StarShow().makeStars();
 	}
-	}}
+	}
 
 
--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -9,13 +9,13 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+speak("iridocyclitis");
 		// 2. Catch the user's answer in a String
-
+String name=JOptionPane.showInputDialog("Write the word I just said.");
 		// 3. If the user spelled the word correctly, speak "correct"
-
+if(name.equalsIgnoreCase("iridocyclitis")) { JOptionPane.showMessageDialog(null, "Correct!");}
 		// 4. Otherwise say "wrong"
-
+else {speak("wrong");}
 		// 5. repeat the process for other words
 		
 	}
